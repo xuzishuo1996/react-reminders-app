090fdd911b2a59e9a6066842691f76e35ad7269f
Zishuo Xu <xuzishuo1996@gmail.com>
2021-08-14 15:27:33 -0400

    add header, footer and note

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+
+    const currYear = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright ⓒ {currYear} </p>
+        </footer>
+    );
+}
+
+export default Footer;
